521e3d0c32c7a579b9a48a48490c6ce73c31a12b
gkarkow <karkowg@live.com>
2012-11-20 11:52:29 -0200

    v0.1

--- tagcloud/src/Main.hs
+++ tagcloud/src/Main.hs
@@ -1,25 +1,21 @@
-{--
-        Esqueleto de programa para gera√ß√£o de bubble cloud em Haskell.
-        Mais informa√ß√µes em: http://www.inf.ufsm.br/~andrea/elc117-2012b
---}
-
+-- T3: Gerando tag clouds em Haskell
 
 module Main where
 
-import Text.Printf -- Oba, Haskell tem printf! :-)
+import Text.Printf
 
-type Point     = (Float,Float)
-type Color     = (Int,Int,Int)
-type Circle    = (Point,Float)
+type Point  = (Float,Float)
+type Color  = (Int,Int,Int)
+type Circle = (Point,Float)
 
 imageWidth :: Int
-imageWidth = 360
+imageWidth = 640
 
 imageHeight :: Int
-imageHeight = 360
+imageHeight = 640
 
 
--- Funcao principal que faz leitura do dataset e gera arquivo SVG
+-- FunÁ„o principal que faz leitura do dataset e gera arquivo SVG
 main :: IO ()
 main = do 
         strcontent <- readFile infile
@@ -37,7 +33,7 @@ readInts :: [String] -> [Int]
 readInts ss = map read ss
 
 
--- Gera o documento SVG da tag cloud, concatenando cabecalho, conteudo e rodape
+-- Gera o documento SVG da tag cloud, concatenando cabeÁalho, conte˙do e rodapÈ
 svgCloudGen :: Int -> Int -> [Int] -> String
 svgCloudGen w h dataset = 
         "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>\n" ++ 
@@ -46,22 +42,34 @@ svgCloudGen w h dataset =
         (concat (svgBubbleGen w h dataset)) ++ "</svg>\n"
 
 
--- Esta funcao deve gerar a lista de circulos em formato SVG.
--- A implementacao atual eh apenas um teste que gera um circulo posicionado no meio da figura.
--- TODO: Alterar essa funcao para usar os dados do dataset.
+-- Esta funÁ„o deve gerar a lista de cÌrculos em formato SVG
+-- A implementaÁ„o atual È apenas um teste que gera um cÌrculo posicionado no meio da figura
+-- TODO: Alterar essa funÁ„o para usar os dados do dataset
 svgBubbleGen:: Int -> Int -> [Int] -> [String]
-svgBubbleGen w h dataset = [svgCircle ((fromIntegral w/2, fromIntegral h/2), 10.0)]
+svgBubbleGen w h dataset = [svgCircle ((fromIntegral w/2, fromIntegral h/2), 10.0) (0,255,0)]
 
 
--- Gera string representando um circulo em SVG. A cor do circulo esta fixa. 
--- TODO: Alterar esta funcao para mostrar um circulo de uma cor fornecida como parametro.
-svgCircle :: Circle -> String
-svgCircle ((x,y),r) = printf "<circle cx=\"%f\" cy=\"%f\" r=\"%f\" fill=\"rgb(255,0,0)\" />\n" x y r
+-- Gera string representando um cÌrculo em SVG. A cor do cÌrculo esta fixa. 
+-- TODO: Alterar esta funÁ„o para mostrar um cÌrculo de uma cor fornecida como par‚metro.
+svgCircle :: Circle -> Color -> String
+svgCircle ((x,y),ra) (r,g,b) = printf "<circle cx=\"%f\" cy=\"%f\" r=\"%f\" fill=\"rgb(%d,%d,%d)\" />\n" x y ra r g b
 
 
--- Configura o viewBox da imagem e coloca retangulo branco no fundo
+-- Configura o viewBox da imagem e coloca um ret‚ngulo branco no fundo
 svgViewBox :: Int -> Int -> String
 svgViewBox w h =
         printf  "<svg width=\"%d\" height=\"%d\" viewBox=\"0 0 %d %d\"" w h w h ++ 
                 " version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\">\n" ++
         printf "<rect x=\"0\" y=\"0\" width=\"%d\" height=\"%d\" style=\"fill:white;\"/>\n" w h
+
+
+-- Calcula a dist‚ncia entre dois pontos
+calcDist :: Point -> Point -> Float
+calcDist (x1,y1) (x2,y2) = sqrt (((x2-x1)^2) + ((y2-y1)^2))
+
+
+-- Verifica intersecÁ„o de cÌrculos
+intersect :: Circle -> Circle -> Bool
+intersect ((x1,y1),r1) ((x2,y2),r2)
+  | calcDist (x1,y1) (x2,y2) >= r1 + r2 = False
+  | otherwise                           = True
