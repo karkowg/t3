8e2547c84138fde37753ed17673bacf1fbf520d5
gkarkow <karkowg@live.com>
2012-11-20 20:29:39 -0200

    v0.2

--- tagcloud/src/Main.hs
+++ tagcloud/src/Main.hs
@@ -18,14 +18,14 @@ imageHeight = 640
 -- Função principal que faz leitura do dataset e gera arquivo SVG
 main :: IO ()
 main = do 
-        strcontent <- readFile infile
-        let pairs = map (span (/= ' ')) (lines strcontent)
-            freqs = readInts (map snd pairs)
-        writeFile outfile (svgCloudGen imageWidth imageHeight freqs)
-        putStrLn "Ok!"
-        where 
-                infile = "dataset.txt"
-                outfile = "tagcloud.svg"
+  strcontent <- readFile infile
+  let pairs = map (span (/= ' ')) (lines strcontent)
+      freqs = readInts (map snd pairs)
+  writeFile outfile (svgCloudGen imageWidth imageHeight freqs)
+  putStrLn "Ok!"
+  where 
+    infile = "dataset.txt"
+    outfile = "tagcloud.svg"
 
 
 -- Transforma lista de strings em lista de inteiros
@@ -36,31 +36,37 @@ readInts ss = map read ss
 -- Gera o documento SVG da tag cloud, concatenando cabeçalho, conteúdo e rodapé
 svgCloudGen :: Int -> Int -> [Int] -> String
 svgCloudGen w h dataset = 
-        "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>\n" ++ 
-        "<!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.1//EN\" \"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\">\n" ++
-        (svgViewBox w h) ++
-        (concat (svgBubbleGen w h dataset)) ++ "</svg>\n"
+  "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>\n" ++ 
+  "<!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.1//EN\" \"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\">\n" ++
+  (svgViewBox w h) ++
+  (concat (svgBubbleGen w h dataset)) ++ "</svg>\n"
 
 
 -- Esta função deve gerar a lista de círculos em formato SVG
 -- A implementação atual é apenas um teste que gera um círculo posicionado no meio da figura
 -- TODO: Alterar essa função para usar os dados do dataset
 svgBubbleGen:: Int -> Int -> [Int] -> [String]
-svgBubbleGen w h dataset = [svgCircle ((fromIntegral w/2, fromIntegral h/2), 10.0) (0,255,0)]
+svgBubbleGen w h dataset = [svgCircle ((fromIntegral w/2, fromIntegral h/2), 64.0) (0,0,255),
+  svgCircle ((fromIntegral w/4, fromIntegral h/2), 16.0) (0,255,0),
+  svgCircle ((fromIntegral w/2, fromIntegral h/4), 32.0) (255,0,0),
+  svgCircle ((fromIntegral w/8, fromIntegral h/2), 32.0) (0,0,155),
+  svgCircle ((fromIntegral w/2 + 160, fromIntegral h/2), 16.0) (0,155,0),
+  svgCircle ((fromIntegral w/2, fromIntegral h/2 + 160), 8.0) (155,0,0)]
 
 
 -- Gera string representando um círculo em SVG. A cor do círculo esta fixa. 
 -- TODO: Alterar esta função para mostrar um círculo de uma cor fornecida como parâmetro.
 svgCircle :: Circle -> Color -> String
-svgCircle ((x,y),ra) (r,g,b) = printf "<circle cx=\"%f\" cy=\"%f\" r=\"%f\" fill=\"rgb(%d,%d,%d)\" />\n" x y ra r g b
+svgCircle ((x,y),ra) (r,g,b) =
+  printf "<circle cx=\"%f\" cy=\"%f\" r=\"%f\" fill=\"rgb(%d,%d,%d)\" />\n" x y ra r g b
 
 
 -- Configura o viewBox da imagem e coloca um retângulo branco no fundo
 svgViewBox :: Int -> Int -> String
 svgViewBox w h =
-        printf  "<svg width=\"%d\" height=\"%d\" viewBox=\"0 0 %d %d\"" w h w h ++ 
-                " version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\">\n" ++
-        printf "<rect x=\"0\" y=\"0\" width=\"%d\" height=\"%d\" style=\"fill:white;\"/>\n" w h
+  printf "<svg width=\"%d\" height=\"%d\" viewBox=\"0 0 %d %d\"" w h w h ++ 
+         " version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\">\n" ++
+  printf "<rect x=\"0\" y=\"0\" width=\"%d\" height=\"%d\" style=\"fill:white;\"/>\n" w h
 
 
 -- Calcula a distância entre dois pontos
